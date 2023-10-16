91d795461286e7fe5d0e9afa95c6b01469b9a486
Damian Kilday <damian@thekildays.com>
2023-10-15 19:53:21 -0600

    Added support for function calls.

--- Services/OAI.cs
+++ Services/OAI.cs
@@ -3,9 +3,11 @@ using TeamGPT.Utilities;
 using OpenAI_API;
 using OpenAI_API.Chat;
 using OpenAI_API.Models;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-using Microsoft.VisualBasic;
+using OpenAI.Builders;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels.SharedModels;
+using OpenAI.Interfaces;
+using OpenAI.Managers;
 
 namespace TeamGPT.Services
 {
@@ -77,7 +79,7 @@ namespace TeamGPT.Services
             
             // Get conversation messages and convert to Betalgo ChatMessage list.
             List<OpenAI.ObjectModels.RequestModels.ChatMessage> messages = new();
-            foreach (ChatMessage message in this.conversation.Messages)
+            foreach (OpenAI_API.Chat.ChatMessage message in this.conversation.Messages)
             {
                 messages.Add(new OpenAI.ObjectModels.RequestModels.ChatMessage(message.Role, message.Content));
             }
@@ -123,12 +125,97 @@ namespace TeamGPT.Services
         {
             List<Thought> thoughts = new();
 
-            foreach (ChatMessage message in conversation.Messages)
+            foreach (OpenAI_API.Chat.ChatMessage message in conversation.Messages)
             {
                 thoughts.Add(toThought((message.Role, message.Content)));
             }
 
             return thoughts;
+        }
+
+        public async Task RunChatFunctionCallTest()
+        {
+            IOpenAIService sdk = bedalgoAiService;
+
+            Console.WriteLine("Chat Function Call Testing is starting:", ConsoleColor.Cyan);
+
+            // example taken from:
+            // https://github.com/openai/openai-cookbook/blob/main/examples/How_to_call_functions_with_chat_models.ipynb
+
+            var fn1 = new FunctionDefinitionBuilder("get_current_weather", "Get the current weather")
+                .AddParameter("location", PropertyDefinition.DefineString("The city and state, e.g. San Francisco, CA"))
+                .AddParameter("format", PropertyDefinition.DefineEnum(new List<string> {"celsius", "fahrenheit"}, "The temperature unit to use. Infer this from the users location."))
+                .Validate()
+                .Build();
+
+            var fn2 = new FunctionDefinitionBuilder("get_n_day_weather_forecast", "Get an N-day weather forecast")
+                .AddParameter("location", new PropertyDefinition {Type = "string", Description = "The city and state, e.g. San Francisco, CA"})
+                .AddParameter("format", PropertyDefinition.DefineEnum(new List<string> {"celsius", "fahrenheit"}, "The temperature unit to use. Infer this from the users location."))
+                .AddParameter("num_days", PropertyDefinition.DefineInteger("The number of days to forecast"))
+                .Validate()
+                .Build();
+            var fn3 = new FunctionDefinitionBuilder("get_current_datetime", "Get the current date and time, e.g. 'Saturday, June 24, 2023 6:14:14 PM'")
+                .Build();
+
+            var fn4 = new FunctionDefinitionBuilder("identify_number_sequence", "Get a sequence of numbers present in the user message")
+                .AddParameter("values", PropertyDefinition.DefineArray(PropertyDefinition.DefineNumber("Sequence of numbers specified by the user")))
+                .Build();
+            try
+            {
+                Console.WriteLine("Chat Function Call Test:", ConsoleColor.DarkCyan);
+                var completionResult = await sdk.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+                {
+                    Messages = new List<OpenAI.ObjectModels.RequestModels.ChatMessage>
+                    {
+                        OpenAI.ObjectModels.RequestModels.ChatMessage.FromSystem("Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."),
+                        OpenAI.ObjectModels.RequestModels.ChatMessage.FromUser("Give me a weather report for Chicago, USA, for the next 5 days.")
+                    },
+                    Functions = new List<FunctionDefinition> {fn1, fn2, fn3, fn4},
+                    // optionally, to force a specific function:
+                    // FunctionCall = new Dictionary<string, string> { { "name", "get_current_weather" } },
+                    MaxTokens = 50,
+                    Model = OpenAI.ObjectModels.Models.Gpt_3_5_Turbo
+                });
+
+                /*  expected output along the lines of:
+                
+                    Message:
+                    Function call:  get_n_day_weather_forecast
+                    location: Chicago, USA
+                    format: celsius
+                    num_days: 5
+                */
+
+                if (completionResult.Successful)
+                {
+                    var choice = completionResult.Choices.First();
+                    Console.WriteLine($"Message:        {choice.Message.Content}");
+
+                    var fn = choice.Message.FunctionCall;
+                    if (fn != null)
+                    {
+                        Console.WriteLine($"Function call:  {fn.Name}");
+                        foreach (var entry in fn.ParseArguments())
+                        {
+                            Console.WriteLine($"  {entry.Key}: {entry.Value}");
+                        }
+                    }
+                }
+                else
+                {
+                    if (completionResult.Error == null)
+                    {
+                        throw new Exception("Unknown Error");
+                    }
+
+                    Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
